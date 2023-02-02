13ca20e1468b7225e4a1a70c517f6c5ff7fbff53
geearevee <110712685+geearevee@users.noreply.github.com>
2023-02-02 18:56:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function func(){
+    alert("WELCOME");
+    let ans = prompt("WHO IS THE BEST FOOTBALLER OF ALL TIME???")
+    if(ans == "MESSI")
+{
+   alert("YOU WON!!!!!!!!!!!!")
+}
+else{
+    alert("YOU LOST !!!! TRY AGAIN!!!!!")
+
+}
+    
+}
+
