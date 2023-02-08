0f7d96912eac07b9293ddaa7af44b5692e216db7
geearevee <110712685+geearevee@users.noreply.github.com>
2023-02-08 19:52:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 function func(){
-    alert("WELCOME");
-    let ans = prompt("WHO IS THE BEST FOOTBALLER OF ALL TIME???")
-    if(ans == "MESSI")
-{
-   alert("YOU WON!!!!!!!!!!!!")
+   let startGame = document.querySelector('#startCount');
+   startGame.setAttribute('disabled', '');
+   startGame.style.border = "3px red solid";
+   let i = 4; 
+   let interval= setInterval(function(){
+       console.log("running internal", i);
+       let element = document.getElementById("timer");
+       console.log(element);
+       i--;
+       element.textContent = i;
+       console.log(i);
+       if(i === 0){
+           clearInterval(interval);
+           document.getElementById("timer").innerHTML = "";
+           startGame.removeAttribute('disabled')
+           startGame.style.border = "none";
+        }
+    },1000)
 }
-else{
-    alert("YOU LOST !!!! TRY AGAIN!!!!!")
 
-}
-    
-}
+
 
